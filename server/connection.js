a05198317b065f5029cd7a67cb3f95820a969337
Thibaut ROUET <thibaut.rouet@yahoo.fr>
2022-03-06 23:56:17 +0100

    3 working methods

--- server/connection.js
+++ server/connection.js
@@ -4,13 +4,21 @@ const { MongoClient } = require('mongodb');
 const MONGODB_URI = "mongodb+srv://clearfashion_user:clearfashion_user@cluster0.g1yua.mongodb.net/clearfashion?retryWrites=true&w=majority";
 const MONGODB_DB_NAME = 'clearfashion';
 
-async function connect() { try { const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); let connected_database = client.db(MONGODB_DB_NAME)        console.log('Connected to database ')        return connected_database } catch (err) { console.error(`Error connecting to the database. \n${err}`); } }//connect()
+async function connect() {
+    try {
+        const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        let connected_database = client.db(MONGODB_DB_NAME)
+        console.log('Connected to database ')
+        return connected_database
+    }
+    catch (err) {
+        console.error(`Error connecting to the database. \n${err}`);
+    }
+}
+//connect()
 
-// Insert the products
-const products = [{ id: "testproduct", name: "name" }, { id: "test2", age: 12 }];
-const dedicated_products = require('./dedicated_scraped.json');
-const montlimart_products = require('./montlimart_scraped.json');
-const adresse_products = require('./adresse_scraped.json');
+
+const all_products = require('./all_scraped.json');
 async function insert_products(products) {
     const db = await connect();
     const collection = db.collection('products');
@@ -18,4 +26,44 @@ async function insert_products(products) {
         const result = collection.insertMany(brand_products);
     }
 }
-insert_products([dedicated_products, montlimart_products, adresse_products])
+//insert_products([all_products])
+
+//Find all products related to a given brand
+brand = "montlimart"
+async function find_by_brand(brand) {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = {
+        brand: brand
+    };
+    collection.find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//find_by_brand(brand)
+
+//Find all products less than a price
+price = 10
+async function find_by_price(price) {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = { price: { $lt: price } }
+    collection.find(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//find_by_price(price)
+
+//Find all products sorted by price (desc)
+async function sort_by_price() {
+    const db = await connect();
+    const collection = db.collection('products');
+    var query = [{ $sort: { "price": -1 } }]
+    collection.aggregate(query).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+//sort_by_price()
