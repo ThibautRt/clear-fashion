78025e4d5acc633d713cc61d78f8c95a9d0c29c6
Thibaut ROUET <thibaut.rouet@yahoo.fr>
2022-03-21 23:56:11 +0100

    vercel added in theory, client not completed

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -46,7 +46,7 @@ const setCurrentProducts = ({ result, meta }) => {
 const fetchProducts = async (page = 1, size = 12) => {
     try {
         const response = await fetch(
-            `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+            `https://serverapi-three.vercel.app?page=${page}&size=${size}`
         );
         const body = await response.json();
 
